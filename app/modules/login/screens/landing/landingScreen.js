95cabc82f01c5346c8786beabc10a3e725fc0c38
asbar <asbar@calcey.com>
2018-09-09 21:27:27 +0530

    finalized realm part 1

--- app/modules/login/screens/landing/landingScreen.js
+++ app/modules/login/screens/landing/landingScreen.js
@@ -2,21 +2,34 @@ import React from 'react';
 import type {
   Element as ReactElement,
 } from 'react';
-import {View, Text} from 'react-native';
-import {Icon} from "@up-shared/components";
+import {View} from 'react-native';
+
+import {Icon} from '@up-shared/components';
+import {customerActions} from '@up-storage/realm';
 
 class LandingScreen extends React.PureComponent<any, any> {
   constructor(props: any) {
     super(props);
   }
 
+  componentDidMount() {
+    customerActions.saveCustomer({
+      custId: 1,
+      custName: 'Asbar Ali',
+      address: 'Colombo, Sri lanka',
+      custImageAddress: 'http:///blahhh',
+    });
+    const customerDetails = customerActions.retrieveAllCustomer();
+    console.log('customerDetails', customerDetails);
+  }
+
   render(): ReactElement<any> {
     return (
       <View>
-        <Icon 
+        <Icon
+          color="green"
           name="icon-share1"
           size={35}
-          color="green"
         />
       </View>
     );
